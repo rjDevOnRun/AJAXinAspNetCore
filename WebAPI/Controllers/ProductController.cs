76cd81964d9740cb6aed61aaf6301933221e02f7
RjDevOnrun <john.roby76@gmail.com>
2021-08-09 19:27:31 +0900

    Add project files.

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebModels;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetAll")]
+        public IEnumerable<Product> GetProducts()
+        {
+            return ProductList();
+        }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public Product GetById(int id)
+        {
+            return ProductList().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<Product> ProductList()
+        {
+            var products = new List<Product>()
+            {
+                new Product(){Id=1, Name="Computer", Price=50000},
+                new Product(){Id=2, Name="Laptop", Price=75000},
+                new Product(){Id=3, Name="Mouse", Price=500},
+                new Product(){Id=4, Name="Keyboard", Price=550},
+                new Product(){Id=5, Name="Monitor", Price=11500}
+            };
+            return products;
+        }
+
+    }
+}
